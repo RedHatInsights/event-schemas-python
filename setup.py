3101bd49188bd1053dc37e98599df2e3b296371b
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-04-20 09:42:19 -0600

    chore: include long description in setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     version='0.1.0',
     url='https://github.com/RedHatInsights/event-schemas-python',
     description='CloudEvents type bindings for console.redhat.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/x-rst',
     packages=find_packages(),
 )

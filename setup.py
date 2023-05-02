7b090784fa8e49a2c78ce03418b80f796ffbba1f
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-05-02 10:19:19 -0600

    chore: fix long description content type

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     url='https://github.com/RedHatInsights/event-schemas-python',
     description='CloudEvents type bindings for console.redhat.com',
     long_description=open('README.md').read(),
-    long_description_content_type='text/x-rst',
+    long_description_content_type='text/markdown',
     packages=find_packages(),
 )
